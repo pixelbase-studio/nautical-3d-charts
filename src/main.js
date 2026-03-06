8b6c39eee2104bb03ee479d8f86590bfe24611e7
coldev01 <124695283+coldev01@users.noreply.github.com>
2026-03-06 08:59:34 +0000

    Fix styles and assets

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import './style.css'
+
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
@@ -6,20 +8,3 @@ AOS.init({
   duration: 600,
   once: true,
 })
-
-// Load partials into all matching elements
-async function loadPartial(selector, file) {
-  const res = await fetch(file)
-  const html = await res.text()
-  
-  document.querySelectorAll(selector).forEach(el => {
-    el.innerHTML = html
-  })
-}
-
-// IIFE to handle async operations
-;(async () => {
-  await loadPartial('#header', 'src/partials/header.html')
-  await loadPartial('.buttons', 'src/partials/buttons.html')
-  await loadPartial('#footer', 'src/partials/footer.html')
-})()
